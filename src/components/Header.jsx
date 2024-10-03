5c4604eada9f4a7bd82b0b07ae865f0fb872bd08
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-10-03 20:44:19 +0700

    implementing header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-
-// logo + name, navigation, cart    
+import styles from "./Header.module.css";
+import dummyLogo from "../assets/dummy_logo.png";
+// logo + name, navigation, cart
 
 const Header = () => {
-    return <>
-    </>
-}
+    return (
+        <>
+            <div className={styles["container"]}>
+                <div className={styles["logo-container"]}>
+                    <img src={dummyLogo} alt="App Logo" className={styles["app-logo"]} />
+                </div>
+                <nav className={styles["navigation-container"]}>
+                    <div className={styles["nav-button-container"]}>Giày nam</div>
+                    <div className={styles["nav-button-container"]}>Giày nữ</div>
+                    <div className={styles["nav-button-container"]}>Giày trẻ em</div>
+                </nav>
+                <div className={styles["action-container"]}></div>
+            </div>
+        </>
+    );
+};
 
 export default Header;
