7728aab115c441e550132aea987bc4bcbbcd427a
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-10-05 15:32:38 +0700

    add mainpage to route

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import MainPage from "./components/MainPage.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
+            {
+                path: "",
+                element: <MainPage />,
+            },
             // view a specific collection
+
             {
                 path: "collections/:collectionName",
             },
